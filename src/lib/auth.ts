5a70bdf4a838c999372f40f74e035448059fc43c
Kyle <kgrahammatzen@gmail.com>
2024-11-15 09:14:00 -0500

    fix: better-auth throwing errors on compile

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,41 +1,36 @@
 import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "./db";
-import { schema } from "./db/schema";
+import { Pool } from "pg";
+import { linear } from "./plugins/linear";
+import { LinearLoginCodeEmail } from "../../emails/linear-code";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg",
-    schema: {
-      ...schema,
-    },
+  database: new Pool({
+    connectionString: process.env.DATABASE_URL,
   }),
-  // emailAndPassword: {
-  //   enabled: true,
-  // },
-  // plugins: [
-  //   linear({
-  //     sendLinearAuth: async ({ email, magicLink, otp, type }, request) => {
-  //       const { data, error } = await resend.emails.send({
-  //         from: "Kyle from Linear <hello@kylegm.com>",
-  //         to: email,
-  //         subject:
-  //           type === "sign-in"
-  //             ? "Sign in to Linear"
-  //             : "Verify your email for Linear",
-  //         react: LinearLoginCodeEmail({ validationCode: otp, magicLink, type }),
-  //       });
+  plugins: [
+    linear({
+      sendLinearAuth: async ({ email, magicLink, otp, type }, request) => {
+        const { data, error } = await resend.emails.send({
+          from: "Kyle from BetterAuth <hello@kylegm.com>",
+          to: email,
+          subject:
+            type === "sign-in"
+              ? "Sign in to BetterAuth"
+              : "Verify your email for BetterAuth",
+          react: LinearLoginCodeEmail({ validationCode: otp, magicLink, type }),
+        });
 
-  //       if (error) {
-  //         console.error("Failed to send email:", error);
-  //         throw new Error("Failed to send email");
-  //       }
+        if (error) {
+          console.error("Failed to send email:", error);
+          throw new Error("Failed to send email");
+        }
 
-  //       console.log("Email sent:", data);
-  //     },
-  //     otpLength: 6,
-  //     disableSignUp: false,
-  //     expiresIn: 300,
-  //   }),
-  // ],
+        console.log("Email sent:", data);
+      },
+      otpLength: 6,
+      disableSignUp: false,
+      expiresIn: 300,
+    }),
+  ],
 });
--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,3 +1,7 @@
+import { loadEnvConfig } from "@next/env";
 import { Resend } from "resend";
+
+// Load environment variables
+loadEnvConfig(process.cwd());
 
 export const resend = new Resend(process.env.RESEND_API_KEY!);
